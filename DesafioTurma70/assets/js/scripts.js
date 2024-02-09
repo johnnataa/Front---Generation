4db8819c6e48fb75e3c81a613b2d950542fa4903
johnnataa <johnnata_@outlook.com>
2024-02-09 14:39:25 -0300

    Adicionando JS

--- DesafioTurma70/assets/js/scripts.js
+++ DesafioTurma70/assets/js/scripts.js
@@ -1,6 +1,10 @@
-﻿const nome = document.querySelector("#nome");
-const email = document.querySelector("#email");
-const mensagem = document.querySelector("#mensagem");
+﻿let nome = document.querySelector("#nome");
+let email = document.querySelector("#email");
+let mensagem = document.querySelector("#mensagem");
+
+nome.style.width = "100%";
+email.style.width = "100%";
+mensagem.style.width = "100%"
 
 let nomeOk = false;
 let emailOk = false;
@@ -8,10 +12,11 @@ let mensagemOk = false;
 let cepOk = false;
 
 function validarNome() {
+  
   let txtNome = document.querySelector("#txtNome");
-
+  
   if (nome.value.length < 3) {
-    txtNome.innerHTML = "Nome muito curto";
+    txtNome.innerHTML = "Nome muito curto!";
     txtNome.style.color = "red";
     nomeOk = false;
   } else {
@@ -35,25 +40,10 @@ function validarEmail() {
   }
 }
 
-function validarEmail2() {
-  let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  let txtEmail = document.querySelector("#txtEmail");
-
-  if (email.value.match(regex)) {
-    txtEmail.innerHTML = "✔";
-    txtEmail.style.color = "green";
-    emailOk = true;
-  } else {
-    txtEmail.innerHTML = "E-mail inválido";
-    txtEmail.style.color = "red";
-    emailOk = false;
-  }
-}
-
 function validarMensagem() {
   let txtMensagem = document.querySelector("#txtMensagem");
 
-  if (mensagem.value.length >= 50) {
+  if (mensagem.value.length >= 100) {
     txtMensagem.innerHTML = "Mensagem muito grande!";
     txtMensagem.style.color = "red";
     mensagemOk = false;
@@ -61,61 +51,5 @@ function validarMensagem() {
     txtMensagem.innerHTML = "✔";
     txtMensagem.style.color = "green";
     mensagemOk = true;
-  }
-}
-
-function enviarForm() {
-  if (nomeOk === true && emailOk === true && mensagemOk === true) {
-    alert(nome.value + ", obrigado pelo contato, aguarde nosso retorno.");
-  } else {
-    alert("Por favor, preencha todos os campos corretamente.");
-  }
-}
-
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
-
-const cepValido = (cep) => cep.length == 8 && eNumero(cep);
-
-function validarCep() {
-  const cep = document.getElementById("cep").value.replace("-", "");
-
-  if (cepValido(cep)) {
-    txtCep.innerHTML = "✔";
-    txtCep.style.color = "green";
-    cepOk = true;
-  } else {
-    txtCep.innerHTML = "CEP Inválido";
-    txtCep.style.color = "red";
-    cepOK = false;
-  }
-}
-
-function consultarCep() {
-  document.getElementById("dados").innerHTML = "";
-console.log(cepOk);
-const cep = document.getElementById("cep").value.replace("-", "");
-const url = `https://viacep.com.br/ws/${cep}/json/`;
-
-if (cepOk === true || cepValido(cep)) {
-   
-    fetch(url)
-      .then((response) => response.json())
-      .then((jsonBody) => {
-        if (jsonBody.erro === true || jsonBody.logradouro === undefined) {
-          alert("CEP nao encontrado!");
-        } else {
-          document.getElementById("dados").innerHTML =
-            jsonBody.logradouro +
-            "\n" +
-            jsonBody.bairro +
-            "\n" +
-            jsonBody.localidade +
-            "\n" +
-            jsonBody.uf;
-        }
-      })
-      .catch((error) => {
-        alert("CEP não encontrado!");
-      });
   }
 }
